6234e7f9027e80245541506839f841a0179ec8b7
Pasquale Paola <pasquale.paola@accenture.com>
2021-02-13 18:40:48 +0100

    first commit

---
+++ src/test/java/it/paspaola/quarkus/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package it.paspaola.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
